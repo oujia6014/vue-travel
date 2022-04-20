cd1b82bda094423ad9d71f4d0b53d7af76c908a7
jiasheng_ou <oujia6014@foxmail.com>
2022-04-20 23:50:31 +0800

    分支测试

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -39,6 +39,7 @@ export default {
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
+      console.log('分支测试')
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
